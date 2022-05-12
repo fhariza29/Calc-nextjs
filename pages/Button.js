380845d96e61fd4c2518a35e1001a1f87567dd38
fhariza29 <fharizaalghifera18634@gmail.com>
2022-05-12 22:25:45 +0100

    mengedit warna button

--- pages/Button.js
+++ pages/Button.js
@@ -4,10 +4,10 @@ const Button = ({val, clickAction, grow = false, operator = false}) => {
   return (
     <button 
     onClick={() => clickAction(val)}
-    className={`text-white text-2xl h-1/5 cursor-pointer  rounded-lg border border-gray-900 hover:bg-gray-700  
+    className={`text-white text-2xl h-1/5 cursor-pointer bg-blue-900 rounded-lg border border-gray-900 hover:bg-gray-700  
     ${grow ? ` flex-row basis-1/2` : `flex-row basis-18 `}
     ${grow && val === 0 ? `flex-[3_0_63%]` : `flex-[1_0_21%] `}
-    ${operator ? `bg-orange-400` : ` bg-blue-900 `}
+   
     
     
     `} 
